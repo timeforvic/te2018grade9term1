f1018a55ec959705999b2a25597055b8177713cb
victor mina <victor.mina@csedge.org>
2018-10-06 15:45:49 -0400

    I completed challenge one.

--- data.js
+++ data.js
@@ -2,4 +2,31 @@
 let message = "Hello World!";
 console.log(message);
 
-// Type your code below!
+let gpa;
+let number;
+number=10;
+let v;
+v=9;
+let i;
+i=8;
+let c="Yurr";
+let t="What up dawg";
+let o="How you been?";
+let r="I like fortnite"
+likesSoup=false;
+likesWilkin=false;
+likesFood=true;
+console.log(number);
+console.log(v);
+console.log(i);
+console.log(c);
+console.log(t);
+console.log(o);
+console.log(r);
+console.log(likesSoup);
+console.log(likesWilkin);
+console.log(likesFood);
+const READLINE=require("readline sync")
+let userinput=READLINE.question("I like chicken");
+console.log('I like chicken:${userinput}');
+console.log(userInput);
