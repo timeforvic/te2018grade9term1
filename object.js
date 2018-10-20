09af9ad8bcd5b68fa7d6d161485728552066b77c
victor mina <victor.mina@csedge.org>
2018-10-20 12:39:38 -0400

    Completed objects

---
+++ object.js
@@ -0,0 +1,16 @@
+let villian = {
+first: "Wilkin",
+last:"De Los Santos",
+catchphrase:"GG",
+greatestCrime:"SpamBuilder"
+};
+
+let hacker = {
+	first: "Samuel",
+	last: "Mina",
+	nickname:"timeforvic",
+	archnemesis:"wilkinway146"
+};
+console.log (`I'm ${villian.first},${villian.last}`);
+console.log (`I'm a ${villian.greatestCrime}`);
+console.log (`I'm ${hacker.nickname}`);
